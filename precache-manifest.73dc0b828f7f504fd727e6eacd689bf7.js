63eb810868b5151c90fa758309cff3236b694960
edgar <edgar.nikogosian98@gmail.com>
2020-08-30 17:17:21 +0400

    Updates

---
+++ precache-manifest.73dc0b828f7f504fd727e6eacd689bf7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0161c5507eafd7cb38d721342ccbf33e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "131c8c79883292e04bd7",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "637d439a354e7b8e2130",
+    "url": "/Portfolio/static/css/main.86ddfbc4.chunk.css"
+  },
+  {
+    "revision": "131c8c79883292e04bd7",
+    "url": "/Portfolio/static/js/2.d125a096.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Portfolio/static/js/2.d125a096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637d439a354e7b8e2130",
+    "url": "/Portfolio/static/js/main.7ca845fe.chunk.js"
+  },
+  {
+    "revision": "49e75da0c6848f9cd251",
+    "url": "/Portfolio/static/js/runtime-main.98e0fd44.js"
+  },
+  {
+    "revision": "d530719ba91f1d0571f20facc3bf3216",
+    "url": "/Portfolio/static/media/2.d530719b.jpg"
+  },
+  {
+    "revision": "38f3750c424e662fe661c3ea99eace38",
+    "url": "/Portfolio/static/media/3.38f3750c.jpg"
+  },
+  {
+    "revision": "322a7f94fefa7cda4bbb5ad591a928c5",
+    "url": "/Portfolio/static/media/4.322a7f94.jpg"
+  },
+  {
+    "revision": "ef56410f0942023c31541e5e1984c89f",
+    "url": "/Portfolio/static/media/5.ef56410f.jpg"
+  },
+  {
+    "revision": "10d0d4ddbf3bf04787934fb7df239a77",
+    "url": "/Portfolio/static/media/8.10d0d4dd.jpg"
+  },
+  {
+    "revision": "4e9425ecf1a165a2fc4713803915964b",
+    "url": "/Portfolio/static/media/9.4e9425ec.jpg"
+  },
+  {
+    "revision": "9947f1d60e368fe4c05699a4ff66ec53",
+    "url": "/Portfolio/static/media/img1.9947f1d6.jpg"
+  },
+  {
+    "revision": "04ea4e2dc9a670fe1c61f38ff16a89e0",
+    "url": "/Portfolio/static/media/img2.04ea4e2d.jpg"
+  },
+  {
+    "revision": "2d967d4abe172fb8f6f8b8d2279baaf5",
+    "url": "/Portfolio/static/media/img3.2d967d4a.jpg"
+  },
+  {
+    "revision": "b53e7856bc8eb9fcd6abc7acf8e412e6",
+    "url": "/Portfolio/static/media/img4.b53e7856.jpg"
+  },
+  {
+    "revision": "aae2c5d069f17b676ddc9774af15d042",
+    "url": "/Portfolio/static/media/img5.aae2c5d0.jpg"
+  },
+  {
+    "revision": "9fba857e6c90d07d509b2e2953aee1eb",
+    "url": "/Portfolio/static/media/img6.9fba857e.jpg"
+  },
+  {
+    "revision": "ec18a8908bc1ce195d6c083304370ace",
+    "url": "/Portfolio/static/media/person3.ec18a890.jpg"
+  },
+  {
+    "revision": "8cd485539478155be2077227b398dded",
+    "url": "/Portfolio/static/media/programing.8cd48553.jpg"
+  },
+  {
+    "revision": "2d967d4abe172fb8f6f8b8d2279baaf5",
+    "url": "/Portfolio/static/media/programingcarusel1.2d967d4a.jpg"
+  },
+  {
+    "revision": "104e08ae59e234086e6c852b16a57715",
+    "url": "/Portfolio/static/media/programingcarusel2.104e08ae.jpg"
+  },
+  {
+    "revision": "1370d5b256732a6859724eaadd3bad7d",
+    "url": "/Portfolio/static/media/programingcarusel3.1370d5b2.jpg"
+  },
+  {
+    "revision": "4fba95f99454bd68b5b2306e3be2f8de",
+    "url": "/Portfolio/static/media/programingcarusel4.4fba95f9.jpg"
+  },
+  {
+    "revision": "d085b3285c4674c5b693b418c4834151",
+    "url": "/Portfolio/static/media/team.d085b328.jpg"
+  }
+]);
