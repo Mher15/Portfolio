0b7285c3aa7ea9883df4b05b5125c8c1744ac274
Mher <mher.janikyan01@gmail.com>
2020-08-31 23:27:36 +0400

    mesigg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React ,{ Component } from "react";
+import React, { Component } from "react";
 import {
     Navbar,
     Nav,
@@ -7,7 +7,7 @@ import {
     Form,
     Button
 } from "react-bootstrap";
-import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "../Pages/Home";
 import Work from "../Pages/Ourworks";
@@ -18,47 +18,49 @@ import Blog from "../Pages/Blog";
 import logo from "./image/logo.png"
 
 export default class Header extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <Router>
-                <Navbar collapseOnSelect expand ="lg" bg="dark" variant ="dark" >
-                    <Container>
-                        <Navbar.Brand href = "/">
-                         <img 
-                            src={logo}
-                            className="logo"
-                         />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls ="responsive-navbar-nav" />
-                        <Navbar.Collapse id ="responsive-navbar-nav">
+                <Router>
+                    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+                        <Container>
+                            <Navbar.Brand href="/">
+                                <img
+                                    src={logo}
+                                    className="logo"
+                                />
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
                                 <Nav className="mr-auto">
-                                    <Link to ="/">Home</ Link>
-                                    <Link to ="/about">About ut</ Link>
-                                    <Link to ="/work">our work</ Link>
-                                    <Link to ="/contacts">Contacts</ Link>
-                                    <Link to ="/blog">Blog</ Link>
+                                    <Link to="/Portfolio">Home</ Link>
+                                    <Link to="/">Home</ Link>
+                                    <Link to="/about">About ut</ Link>
+                                    <Link to="/work">our work</ Link>
+                                    <Link to="/contacts">Contacts</ Link>
+                                    <Link to="/blog">Blog</ Link>
                                 </Nav>
                                 <Form inline>
-                                    <FormControl 
+                                    <FormControl
                                         type="text"
-                                        placeholder ="Search"
-                                        className ="mr-sm-2"
-                                        />
-                                        <Button variant ="outline-info">Search</Button>
+                                        placeholder="Search"
+                                        className="mr-sm-2"
+                                    />
+                                    <Button variant="outline-info">Search</Button>
                                 </Form>
 
-                        </Navbar.Collapse>
+                            </Navbar.Collapse>
 
-                    </Container>
-                </Navbar>
-                
+                        </Container>
+                    </Navbar>
+
                     <Switch>
-                        <Route exact path ="/" component= {Home}/>
-                        <Route exact path ="/about" component= {About}/>
-                        <Route exact path ="/work" component= {Work}/>
-                        <Route exact path ="/contacts" component= {Contact}/>
-                        <Route exact path ="/blog" component= {Blog}/>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/Portfolio" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/work" component={Work} />
+                        <Route exact path="/contacts" component={Contact} />
+                        <Route exact path="/blog" component={Blog} />
                     </Switch>
                 </Router>
             </>
