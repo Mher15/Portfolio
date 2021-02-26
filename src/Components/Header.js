d8c0ab5598e6822c405a022866a96b3ecdeb76ec
Mher15 <68960611+Mher15@users.noreply.github.com>
2021-02-26 23:45:56 +0400

    fix Navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -24,7 +24,7 @@ export default class Header extends Component {
                 <Router>
                     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
                         <Container>
-                            <Navbar.Brand href="/Portfolio">
+                            <Navbar.Brand href="/">
                                 <img
                                     src={logo}
                                     className="logo"
