cfa895ce4702a7b16812125924576632b39bd6bb
Mher15 <68960611+Mher15@users.noreply.github.com>
2021-02-26 23:37:18 +0400

    Fix NavLink in menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -33,7 +33,7 @@ export default class Header extends Component {
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                             <Navbar.Collapse id="responsive-navbar-nav">
                                 <Nav className="mr-auto nav">
-                                    <Link className="link" to="/">Home</ Link>
+                                    <Link className="link" exact to="/">Home</ Link>
                                     <Link className="link"to="/about">About us</ Link>
                                     <Link className="link"to="/work">Our work</ Link>
                                     <Link className="link"to="/contacts">Contacts</ Link>
